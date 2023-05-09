4bd7603f246a169915be6b3392500ad270c648fd
crayfish <hentai121@163.com>
2023-05-09 21:20:23 +0800

    add other cutout method

--- photo/cutouts.go
+++ photo/cutouts.go
@@ -13,6 +13,7 @@ import (
 
 type Cutouts struct {
 	rmbgKey string
+	pixianKey string
 	path string
 	C chan map[int64]string
 }
@@ -20,12 +21,82 @@ type Cutouts struct {
 func NewCutouts() *Cutouts {
 	return &Cutouts{
 		rmbgKey: goconf.VarStringOrDefault("", "photo", "removebg", "apikey"),
+		pixianKey: goconf.VarStringOrDefault("", "photo", "pixian", "authorization"),
 		C: make(chan map[int64]string, 5),
 		path: goconf.VarStringOrDefault("", "photo", "path"),
 	}
 }
 
 func (t *Cutouts) RemoveBackground(id int64, uri string) {
+	if t.pixianKey != "" {
+		t.pixian(id, uri)
+	}
+	if t.rmbgKey != "" {
+		t.removebg(id, uri)
+	}
+	
+}
+
+func (t *Cutouts) pixian(id int64, uri string) {
+	form := new(bytes.Buffer)
+	writer := multipart.NewWriter(form)
+	formField, err := writer.CreateFormField("image.url")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = formField.Write([]byte(uri))
+
+	writer.Close()
+
+	filename := fmt.Sprintf("%suser_%d.jpg", t.path, id)
+	if _, err := os.Stat(t.path); os.IsNotExist(err) { // 检查目录是否存在
+		err := os.Mkdir(t.path, os.ModePerm) // 创建目录
+		if err != nil {
+			fmt.Println("创建本地临时文件失败")
+			return
+		}
+	}
+
+	r, err := http.NewRequest(http.MethodPost, "https://api.pixian.ai/api/v1/remove-background", form)
+	if err != nil {
+		fmt.Println("Remove Background请求生成失败")
+		return
+	}
+
+	r.Header.Add("Authorization", t.pixianKey)
+	r.Header.Set("Content-Type", writer.FormDataContentType())
+	
+
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		fmt.Println("Remove Background请求发送失败")
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return
+	}
+
+	file, err := os.Create(filename) // 创建本地文件
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body) // 将响应体中的数据写入文件
+	if err != nil {
+		return
+	}
+
+	t.C <- map[int64]string{
+		id:filename,
+	}
+	
+}
+
+func (t *Cutouts) removebg(id int64, uri string) {
 	form := new(bytes.Buffer)
 	writer := multipart.NewWriter(form)
 	formField, err := writer.CreateFormField("image_url")
@@ -88,5 +159,4 @@ func (t *Cutouts) RemoveBackground(id int64, uri string) {
 	t.C <- map[int64]string{
 		id:filename,
 	}
-	
 }
