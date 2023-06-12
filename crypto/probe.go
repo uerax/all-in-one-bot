6a8b94c7ee974ab3e1c8295b08c89cb461822822
wesker <uerax@live.com>
2023-06-13 05:25:58 +0800

    Optimize experience

--- crypto/probe.go
+++ crypto/probe.go
@@ -109,11 +109,29 @@ func (t *Probe) KLineProbe(crypto string, ctx context.Context) {
 }
 
 func (p *Probe) MemePrice(query string, chain string) {
-	pair := p.api.MemePrice(query, chain)
-	if pair == nil {
-		p.Meme <- "查询失败,请检查参数"
-		return
+
+	pair := new(Pair)
+	if chain == "" {
+		chain = "eth"
+		pair = p.api.MemePrice(query, chain)
+		if pair == nil {
+			chain = "bsc"
+			pair = p.api.MemePrice(query, chain)
+			if pair == nil {
+				p.Meme <- "没有查询到相关合约"
+				return
+			}
+		}
+	} else {
+		pair = p.api.MemePrice(query, chain)
+		if pair == nil {
+			p.Meme <- "查询失败,请检查参数"
+			return
+		}
 	}
+
+
+	
 	s := fmt.Sprintf("*%s:$%s* \n*Chain:* %s | *Price:* $%s\n\n*5M:*    %0.2f%%    $%0.2f    %d/%d\n*1H:*    %0.2f%%    $%0.2f    %d/%d\n*6H:*    %0.2f%%    $%0.2f    %d/%d\n*1D:*    %0.2f%%    $%0.2f    %d/%d\n\n", pair.BaseToken.Name, pair.BaseToken.Symbol, pair.ChainId, pair.PriceUsd, pair.PriceChange.M5, pair.Volume.M5, pair.Txns.M5.B, pair.Txns.M5.S, pair.PriceChange.H1, pair.Volume.H1, pair.Txns.H1.B, pair.Txns.H1.S, pair.PriceChange.H6, pair.Volume.H6, pair.Txns.H6.B, pair.Txns.H6.S ,pair.PriceChange.H24, pair.Volume.H24, pair.Txns.H24.B, pair.Txns.H24.S)
 
 	check := p.api.MemeCheck(query, chain)
