65f268bec76546a707511c8b3b065033615e65dc
wesker <uerax@live.com>
2023-06-18 04:31:37 +0800

    add immediate ticker

--- config/config.go
+++ config/config.go
@@ -6,14 +6,11 @@ import (
 	"github.com/uerax/goconf"
 )
 
-func init() {
-	flag.Parse()
-}
-
 var (
 	path = flag.String("c", "all-in-one-bot.yml", "项目的配置文件地址(使用绝对路径) 例: -c /etc/all-in-one-bot.yml")
 )
 
 func Load() {
+	flag.Parse()
 	goconf.LoadConfig(*path)
 }
