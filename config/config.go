fe243e579a49bd76024b204fa85d300e6bcbce13
wesker <uerax@live.com>
2023-06-19 04:01:37 +0800

    add version cmd

--- config/config.go
+++ config/config.go
@@ -1,16 +1,9 @@
 package config
 
 import (
-	"flag"
-
 	"github.com/uerax/goconf"
 )
 
-var (
-	path = flag.String("c", "all-in-one-bot.yml", "项目的配置文件地址(使用绝对路径) 例: -c /etc/all-in-one-bot.yml")
-)
-
-func Load() {
-	flag.Parse()
-	goconf.LoadConfig(*path)
+func Load(path string) {
+	goconf.LoadConfig(path)
 }
