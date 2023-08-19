37672840912b2dd8e3c66f620cd8a61a75b11b8b
wesker <uerax@live.com>
2023-08-20 05:46:38 +0800

    fix: WalletTxInterestRate display error bug

--- tg/cmd.go
+++ tg/cmd.go
@@ -17,7 +17,7 @@ func walletTxInterestRate(args string) {
 		return
 	}
 	if len(arg) == 1 {
-		arg = append(arg, "100")
+		arg = append(arg, "50")
 	}
 
 	go api.Track.WalletTxInterestRate(arg[0], arg[1], false)
