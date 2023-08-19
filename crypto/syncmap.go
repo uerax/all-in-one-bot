bb3247f4e197d1a414955b6933b0630a0a4810ba
wesker <uerax@live.com>
2023-08-20 05:09:16 +0800

    feat: add WalletTxInterestRate

--- crypto/syncmap.go
+++ crypto/syncmap.go
@@ -7,6 +7,13 @@ type SyncMap struct {
 	M  map[string]any
 }
 
+func NewSyncMap() *SyncMap {
+	return &SyncMap{
+		Mu: sync.Mutex{},
+		M:  make(map[string]any),
+	}
+}
+
 func (t *SyncMap) Swap(addr string, v any) {
 	t.Mu.Lock()
 	defer t.Mu.Unlock()
