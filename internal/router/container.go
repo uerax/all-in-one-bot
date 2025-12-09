9a10119092cac6541c046ba12e9e5fb4b928b584
wesker <uerax@live.com>
2025-12-10 03:48:42 +0800

    feat: Basic framework completed

--- internal/router/container.go
+++ internal/router/container.go
@@ -1,8 +1,12 @@
 package router
 
-import "github.com/uerax/all-in-one-bot/lite/internal/pkg/logger"
+import (
+	"github.com/uerax/all-in-one-bot/lite/internal/config"
+	"github.com/uerax/all-in-one-bot/lite/internal/pkg/logger"
+)
 
 // Dependencies 结构体持有所有 Handler 可能需要的服务（例如 DB连接、外部API客户端）。
 type Dependencies struct {
+	Config *config.Config
 	Logger logger.Log
 }
